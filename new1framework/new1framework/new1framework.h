b4fc44bd59aae78689e0ca2df8873f0d76addd95
AjaY… <ajay....@AjaYs-MacBook-Air.local>
2019-02-24 17:52:39 +0530

    ajay

---
+++ new1framework/new1framework/new1framework.h
@@ -0,0 +1,19 @@
+//
+//  new1framework.h
+//  new1framework
+//
+//  Created by AjaY….. on 24/02/19.
+//  Copyright © 2019 AjaY…. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for new1framework.
+FOUNDATION_EXPORT double new1frameworkVersionNumber;
+
+//! Project version string for new1framework.
+FOUNDATION_EXPORT const unsigned char new1frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <new1framework/PublicHeader.h>
+
+
---
+++ new1framework/new1framework/webservice.swift
@@ -0,0 +1,16 @@
+//
+//  webservice.swift
+//  new1framework
+//
+//  Created by AjaY….. on 24/02/19.
+//  Copyright © 2019 AjaY…. All rights reserved.
+//
+
+import Foundation
+
+public class sevice {
+    private init(){}
+    public static func stringNew() -> String{
+       return "ajay"
+    }
+}
